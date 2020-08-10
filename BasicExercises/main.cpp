0f917a620f406e354e109fa162a1f24a89b1f0cb
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-08-10 11:12:41 +0300

    loops update

--- BasicExercises/main.cpp
+++ BasicExercises/main.cpp
@@ -19,5 +19,42 @@ int main() {
     std::cout << Sum(3,5) << std::endl;
     SumAndLog(3,5);
 
+    Log("--------------------------");
+    int a = 8;
+    std::cout << a++ << std::endl; // first makes operation on variable than increases it
+    std::cout << ++a << std::endl; // first increase than operate
+    Log("--------------------------");
+
+    Log("IF ELSE Examples");
+    bool boolean = true;
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (boolean)
+        {
+            Log("Boolean is true");
+            boolean = false;
+        }
+        else
+            Log("Boolean is false");
+    }
+    Log("--------------------------");
+    Log("For and While Loops");
+
+    for (int i = 0; i < 5; i++) {
+        Log("Hello World!");
+    }
+
+    int x = 0;
+
+    while(x<5)
+    {
+        std::cout << x << std::endl;
+        x++;
+
+    }
+
+    Log("--------------------------");
+
     std::cin.get();
 }
